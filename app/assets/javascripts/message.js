4ac79a3393df212919c91babdd46e511b16dadcd
ararechan <59217931+ararechan@users.noreply.github.com>
2020-01-16 20:07:35 +0900

    ajax_14

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,25 @@
 $(function(){ 
   function buildHTML(message){
-   if ( message.image ) {
+    if ( message.image ) {
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+            <img src=${message.image} >
+          </div>
+        </div>`
+      return html;
+    } else {
      var html =
       `<div class="message" data-message-id=${message.id}>
          <div class="upper-message">
@@ -15,30 +34,11 @@ $(function(){
            <p class="lower-message__content">
              ${message.content}
            </p>
-           <img src=${message.image} >
          </div>
        </div>`
-     return html;
-   } else {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-       </div>`
-     return html;
-   };
- }
+      return html;
+    };
+  }
 $('#new_message').on('submit', function(e){
   e.preventDefault();
   var formData = new FormData(this);
@@ -51,15 +51,15 @@ $('#new_message').on('submit', function(e){
     processData: false,
     contentType: false
   })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-      $('form')[0].reset();
-      $('input').prop('disabled', false);
-    })
-    .fail(function() {
-      alert("メッセージ送信に失敗しました");
+  .done(function(data){
+    var html = buildHTML(data);
+    $('.messages').append(html);
+    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    $('form')[0].reset();
+    $('input').prop('disabled', false);
+  })
+  .fail(function() {
+    alert("メッセージ送信に失敗しました");
   });
 })
 });
