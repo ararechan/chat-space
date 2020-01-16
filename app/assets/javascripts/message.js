f6aa16013b00841ba68ae708cf7df559db6af4c4
ararechan <59217931+ararechan@users.noreply.github.com>
2020-01-16 18:16:09 +0900

    ajax_9

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -54,6 +54,7 @@ $('#new_message').on('submit', function(e){
   .done(function(data){
     var html = buildHTML(data);
     $('.messages').append(html);
+    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
     $('form')[0].reset();
   })
 })
